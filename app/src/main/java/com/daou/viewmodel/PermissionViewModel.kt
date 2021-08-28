5ba157411372fdcb5a819c79cae2221ec875e22c
이호근인턴사원 <h0rms@daou.co.kr>
2021-08-28 21:41:57 +0900

    안드로이드 버전별 권한부여

---
+++ app/src/main/java/com/daou/viewmodel/PermissionViewModel.kt
@@ -0,0 +1,7 @@
+package com.daou.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.daou.repository.Repository
+
+class PermissionViewModel(private val repository: Repository) : ViewModel() {
+}
